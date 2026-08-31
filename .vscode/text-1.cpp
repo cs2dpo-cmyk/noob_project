d2c8a8ac567d8e4024e439be64bdb1ac04742c1e
cs2dpo <guo790605@gmail.com>
2026-08-31 11:13:10 +0800

    建立還原點

---
+++ .vscode/text-1.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+int main(){
+using namespace std;
+    cout<<"請輸入數字"<<endl;
+    int a ;
+    cin>>a;
+    cout<<"你輸入的數字是:"<<a<<endl;
+    cout<<"Hello World!"<<endl;
+    return 0; 
+} 
